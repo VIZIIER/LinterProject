76dd4f5ed583837f49037c569d477aac1e3fb157
Sultan <105415164+VIZIIER@users.noreply.github.com>
2025-03-13 05:02:01 +0300

    Update utils.py

--- src/utils.py
+++ src/utils.py
@@ -1,6 +1,5 @@
 import subprocess
 
 def run_command(command):
-    # Run a command and return its output.
     process = subprocess.run(command, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
     return process.stdout
